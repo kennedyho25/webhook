25ea39459200d2a7ee467a8589b3d4b626d9d73a
Ken <kennedyho25@gmail.com>
2018-10-15 02:15:31 +0800

    first commit

---
+++ Config.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+	//dbUrl:"mongodb://mikro_admin:plz3n732474@ds163650.mlab.com:63650/mikroniaga"
+ 	dbUrl:"mongodb://kennedyho:kennedy123@ds247678.mlab.com:47678/mf_glh_siliver"
+}
